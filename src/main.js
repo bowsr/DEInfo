e473100b6f4ce4c3da32d1cd9bb2215592b78a91
bowsr <bowserm@outlook.com>
2021-04-22 00:33:14 -0700

    And removed redundant react components from main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { setupDamageData, getValue, getExtraInfoValue, getBaseWeapons, getSingleBaseWeapon, getModifiersWithVersionTarget, translateID, translateVersionID, Demon } from './dataHandling';
+import { ArmoredBaron } from './components/demons';
+import { VersionSelector } from './components/navigation';
+import { setupDamageData, getBaseWeapons, getSingleBaseWeapon, translateID, translateVersionID } from './data/dataHandling';
 
 /*
 function ArmoredBaron(props) {
@@ -15,62 +17,6 @@ function ArmoredBaron(props) {
     );
 }
 */
-
-function DemonBaseInfo(props) {
-    var name = translateID(props.demonID);
-    var hp = getValue(props.demonID, 'health', props.versions, props.target);
-    var stagger = Math.trunc(hp * getValue(props.demonID, 'stagger', props.versions, props.target));
-    return (
-        <div>
-            <h2>{name}</h2>
-            <h3>Stats</h3>
-            <p>Health: {hp}</p>
-            <p>Stagger Threshold: {stagger}</p>
-        </div>
-    )
-}
-
-function DemonDamageModifiers(props) {
-
-}
-
-function TooLowVersionWarning(props) {
-    if(props.target >= props.intro) return (<div></div>);
-    return (
-        <div>
-            <div className='lowVersionWarning'>Data does not exist for the version you selected ({translateVersionID(props.target)})</div>
-            <div className='lowVersionWarning'>Showing data for version {translateVersionID(props.intro)} instead</div>
-        </div>
-    )
-}
-
-class ArmoredBaron extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            armorHP: getExtraInfoValue('baron_armored', 'armorHP', this.props.versions),
-            armorModifiers: getModifiersWithVersionTarget(this.props.versions, 'baron_armored', false, 50),
-            modifiers: getModifiersWithVersionTarget(this.props.versions, 'baron', false, 50),
-            intro: 50
-        };
-    }
-
-    render() {
-        const { name, hp, stagger, armorHP, armorModifiers, modifiers, intro } = this.state;
-        const selectedVersion = (this.props.selectedVersion < intro) ? intro : this.props.selectedVersion;
-        return (
-            <div>
-                <TooLowVersionWarning target={this.props.selectedVersion} intro={intro} />
-                <DemonBaseInfo demonID={'baron_armored'} versions={this.props.versions} target={selectedVersion} />
-                <p>Armor HP: {armorHP}</p>
-                <h3>Damage against Armor</h3>
-                <div>
-
-                </div>
-            </div>
-        );
-    }
-}
 
 class Shotgun extends React.Component {
     constructor(props) {
@@ -97,29 +43,6 @@ class Shotgun extends React.Component {
                 <p>Total Damage (Point Blank): {pellets * pb}</p>
             </div>
         )
-    }
-}
-
-class VersionSelector extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        this.props.onVersionChange(event.target.value);
-    }
-
-    render() {
-        var selections = [];
-        this.props.versionIDs.forEach(id => {
-            selections.push(<option key={id} value={id}>{translateVersionID(id)}</option>)
-        });
-        return (
-            <select value={this.props.selectedVersion} onChange={this.handleChange}>
-                {selections}
-            </select>
-        );
     }
 }
 
