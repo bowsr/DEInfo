361721e6fda2f014fb36181cac6a67ec4a95a623
bowsr <bowserm@outlook.com>
2021-04-21 22:57:19 -0700

    Added version target parameter to getValue to support cases of stats differing per version (e.g. Blood Maykr HP)

--- src/jsx/dataHandling.js
+++ src/jsx/dataHandling.js
@@ -202,14 +202,14 @@ function getNextVersionFromPrev(versions, prevID) {
 }
 
 // For getting values like health/stagger/armor (stuff that isn't stored in Maps)
-function getValue(demon, key, versions, current = null) {
+function getValue(demon, key, versions, target, current = null) {
     if(current === null) current = getCurrentPatch(versions);
     if((key === 'health' || key === 'stagger')) {
         if(demon === 'baron_armored') demon = 'baron';
         if(demon === 'prowler_cursed') demon = 'prowler';
     }
     var d = current.getDemon(demon);
-    if(d === undefined) return (current.prev === -1) ? null : getValue(demon, key, versions, getVersionFromID(versions, current.prev));
+    if(d === undefined) return (current.prev === -1) ? null : getValue(demon, key, versions, target, getVersionFromID(versions, current.prev));
     var value;
     switch(key) {
         case 'health':
@@ -224,7 +224,8 @@ function getValue(demon, key, versions, current = null) {
         default:
             return null;
     }
-    if(value === null) return (d.intro) ? null : getValue(demon, key, versions, getVersionFromID(versions, current.prev));
+    if(value === null) return (d.intro) ? null : getValue(demon, key, versions, target, getVersionFromID(versions, current.prev));
+    if(!d.intro && current.id > target) return getValue(demon, key, versions, target, getVersionFromID(versions, current.prev))
     return value;
 }
 
